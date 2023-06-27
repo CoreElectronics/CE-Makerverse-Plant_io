c31738c06e732374ec65ea48ca9b12124d900d5d
IT3-4tk3 <it3.4tk3@coreelectronics.com.au>
2023-06-27 15:56:39 +1000

    reformat test moisture sensor

--- code/test_moisture_sensor.py
+++ code/test_moisture_sensor.py
@@ -1,14 +1,23 @@
-# Test the Soil Moisture Sensor
-# This script simply prints the soil moisture to the shell so you can experiment with
-# the sensor placement and make sure the sensor is working properly.
+"""
+test_moisture_sensor.py
+
+Simply print the soil moisture to the shell every 100ms so you can experiment with
+the sensor placement and make sure the sensor is working properly
+"""
 
 from time import sleep_ms
 from Plant_io import Plant_io
 
-# Initialise the Plant_io controller
-plant = Plant_io() 
 
-while True:
-    current_soil_moisture = plant.measure_soil()
-    print(f'moisture {current_soil_moisture:5.2f}%')
-    sleep_ms(100)
+def main():
+    """
+    Every 100ms print the measured soil moisture
+    """
+    plant = Plant_io()
+    while True:
+        print(f"moisture {plant.measure_soil():5.2f}%")
+        sleep_ms(100)
+
+
+if __name__ == "__main__":
+    main()
